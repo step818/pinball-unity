aa330ecb483c4fa80fe6cfd1772341d86004cd57
Ian Christner <ichristn@ucsc.edu>
2019-05-21 15:52:58 -0700

    Updated Collision to keep track of global Score

--- Assets/HitScript.cs
+++ Assets/HitScript.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Score;
 
 public class HitScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Collision Function
+    void OnCollisionEnter(Collision col)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (col.gameObject.name == "HomeRunMid")
+        {
+            ScoreScript.score += 1000;
+            Debug.Log(ScoreScript.score);
+        }
+        if (col.gameObject.name == "HomeRunLeft")
+        {
+            ScoreScript.score += 300;
+            Debug.Log(ScoreScript.score);
+        }
+        if (col.gameObject.name == "HomeRunRight")
+        {
+            ScoreScript.score += 300;
+            Debug.Log(ScoreScript.score);
+        }
+        if (col.gameObject.name == "Mid")
+        {
+            ScoreScript.score += 20;
+            Debug.Log(ScoreScript.score);
+        }
+        if (col.gameObject.name == "BuntMid")
+        {
+            ScoreScript.score += 10;
+            Debug.Log(ScoreScript.score);
+        }
+        if (col.gameObject.name == "MidRight")
+        {
+            ScoreScript.score += 50;
+            Debug.Log(ScoreScript.score);
+        }
+        if (col.gameObject.name == "MidLeft")
+        {
+            ScoreScript.score += 50;
+            Debug.Log(ScoreScript.score);
+        }
     }
 }
