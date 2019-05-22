6d68ee8f441a0935f4623028b14655560a9be360
ScoutPilgrim <ianchristner@gmail.com>
2019-05-22 15:58:16 -0700

    Added Score too score board

---
+++ Assets/ScoreText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Score;
+
+public class ScoreText : MonoBehaviour
+{
+    public TextMesh scoreText;
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + ScoreScript.score;
+    }
+}
