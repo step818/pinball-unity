22ad92e0545ed20e05199ac137712c56b6c148e1
Stephen Trewick <stephent818@gmail.com>
2019-05-22 14:40:33 -0700

    lives and gameover. time stops

---
+++ Assets/PlayerLives/LoseLife.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseLife : MonoBehaviour
+{
+    void OnTriggerEnter (Collider col)
+    {
+        Debug.Log("Object entered the trigger");
+        GameControlScript.health -= 1;
+    }
+}
