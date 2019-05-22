b5883983a4b9a9ee86ce65f1ef179f60b39865a8
unknown <crystal_fecteau@ymail.comgit config --global user.name Crystal>
2019-05-22 15:36:56 -0700

    add bats, sound script, and flipper scripts for particles

---
+++ Assets/FlipperRightPart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperRightPart : MonoBehaviour
+{
+    public ParticleSystem myPart;
+
+    private void Start()
+    {
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Ball")
+        {
+            StartPart(col);
+        }
+    }
+    void StartPart(Collision col)
+    {
+        myPart.Play();
+    }
+}
