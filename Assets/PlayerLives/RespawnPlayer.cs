35543ca614b07dcb4ef9cb2a10e563ba405fd74b
Stephen Trewick <stephent818@gmail.com>
2019-05-22 11:38:50 -0700

    PlayerLives folder with RespawnPlayer script

---
+++ Assets/PlayerLives/RespawnPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (transform.position.z < -16)
+            Respawn();
+    }
+
+    private void Respawn()
+    {
+        transform.position = new Vector3(3, 0.228f, -2);
+
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+
+    }
+}
